7c45863e2537eb7a4c18a253e42c09736a4a8bb6
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2024-04-08 23:55:51 +0100

    updated the whole project again

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../Header.jsx'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Error = () => {
   return (
